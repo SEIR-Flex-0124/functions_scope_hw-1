c96c113e2e033718aa10d54409949b3dbab51653
kerBamf <zach.pedigo@gmail.com>
2023-02-01 19:14:03 -0500

    finished problem 3

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 // 1.1. What is the difference between a parameter and an argument?
-
+    //A parameter acts as a placeholder for arguments to be passed when the function is called
 // 1.2. Within a function, what is the difference between return and console.log?
+    //Console.log sends text to the console. Return provides a value that can be used at other points in the code. i.e. "let randVariable = randFunction(arg1, arg2);" sets randVariable to the value returned when calling randFunction.
 
 // 1. 3. What are the implications of the ability of a function to return a value?
+    //By having a function return a value, it can be used to define variables, manipulate given values, and allow repeated calculations to be peformed as many times as necessary, while only writing out the calculation once.
 
 // 2. calculateCube
 function calculateCube(num) {
     // YOUR CODE HERE
+    return(Math.pow(num, 3));
 }
+console.log(calculateCube(5));
 
 // 3. isAVowel
 function isAVowel(letter) {
     // YOUR CODE HERE
-}
+    return(letter == "a"||letter == "e"||letter == "i"||letter == "o"||letter == "u");
+    }
+console.log(isAVowel("a"));
 
 // 4. getTwoLengths
 function getTwoLengths(word1, word2) {
